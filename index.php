91629063c231dee7636f98b5ff02fdbb945c0c5f
Ali Sheidaei <ali.sheidaei.k@gmail.com>
2022-04-15 22:28:11 -0600

    Add page.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ while(have_posts()) : the_post();?>
 
 <article class="post"> 
     <h2> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a></h2>
-    <h2>Index</h2>
+
     <div> <?php the_content(); ?> </div>
 
     <p>This post was written by <?php the_author(); ?> at <?php the_date();?></p>
